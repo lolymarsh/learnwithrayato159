88db452946f2ad5ae2be6f8a80251e42750e7b0d
lolymarsh <marshloly@gmail.com>
2023-12-21 15:06:38 +0700

    section 8

--- pkg/databases/db.go
+++ pkg/databases/db.go
@@ -1,1 +1,19 @@
 package databases
+
+import (
+	"log"
+	"lolyshop/config"
+
+	"github.com/jmoiron/sqlx"
+)
+
+func DbConnect(cfg config.IDbConfig) *sqlx.DB {
+	db, err := sqlx.Connect("pgx", cfg.Url())
+	if err != nil {
+		log.Fatalf("connect to database failed: %v", err)
+	}
+
+	db.DB.SetMaxOpenConns(cfg.MaxOpenConns())
+
+	return db
+}
